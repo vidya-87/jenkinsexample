4b30c3f4b9806e3800f5901081c1a0d0728bb7cc
sweet <sweet@LAPTOP-GUIQBH4F.home>
2024-01-16 13:07:21 +0000

    commit

---
+++ tunneling/src/tunneling/Helloguys.java
@@ -0,0 +1,18 @@
+package tunneling;
+
+public class Helloguys {
+
+public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Hello Java");
+		
+		System.out.println("Hi Java");
+
+		System.out.println("Hi girls");
+
+		// TODO Auto-generated method stub
+
+	}
+}
