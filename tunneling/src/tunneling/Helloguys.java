618c71feee1568e37c18005098f7d58dded4fce9
vidya-87 <101112830+vidya-87@users.noreply.github.com>
2024-01-16 15:14:06 +0000

    Update Helloguys.java

--- tunneling/src/tunneling/Helloguys.java
+++ tunneling/src/tunneling/Helloguys.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
 		
 		System.out.println("Hi Java");
 
-		System.out.println("Hi girls");
+		System.out.println("Hi girls and boys");
 
 		// TODO Auto-generated method stub
 
